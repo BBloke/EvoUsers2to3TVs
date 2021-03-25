e469fb9220461f5919380eace8424e49e835996b
BBloke <bbloke@blueyonder.co.uk>
2021-03-25 17:56:33 +0000

    Update userUpdate.inc.php

--- assets/snippets/userupdate/userUpdate.inc.php
+++ assets/snippets/userupdate/userUpdate.inc.php
@@ -14,8 +14,6 @@ $adminid = 576; // Not always ID 1 so please checkdate
 // 1 = yes
 $updateExistingRoles = 0;
 
-// Table names
-
 // You may wish to change the attributes_extended table depending on your old system setup.
 
 $tbExtended 		= $modx->db->config['table_prefix'] . "web_user_attributes_extended";
@@ -28,138 +26,158 @@ $tbUserValues		= $modx->db->config['table_prefix'] . "user_values";
 $tbUserRoleVar 		= $modx->db->config['table_prefix'] . "user_role_vars";
 $tbSiteTVs 			= $modx->db->config['table_prefix'] . "site_tmplvars";
 
-// Get the extended attribute field names and create TVs
-$sql = "SHOW COLUMNS FROM ". $tbExtended .";";
-$rs = $modx->db->query($sql);
-
-while ( $row = $modx->db->getRow($rs) )
+if ( $_GET['update'] == 'view' || $_GET['commit'] == 'yes')
 {
-	$fields [] = $row['Field'];
-}
-
-unset($fields[0]);
-unset($fields[1]);
-
-echo "<h1>Getting Fields</h1>";
-
-print_r($fields);
-
-
-// Create new TV's based on above field list:
-echo "<h1>Inserting new TVs</h1>";
-foreach ( $fields as $field )
-{	
-	$insertflds = array(
-					'type' => 'text',
-					'name' => $field,
-					'caption' => $field,
-					'description' => $field,
-					'editor_type' => 0,
-					'category' => 0,
-					'editedon' => 0,
-					'createdon' => 0,
-					'locked' => 0);
-
-	print_r($insertflds);
-	echo "<br />";
 	
-	$modx->db->insert($insertflds, $tbSiteTVs );
-	$newId = $modx->db->getInsertId();
+	echo "Parametes Using:<br />";
+	echo $tbUsers."<br />";
+	echo $tbUserSettings."<br />";
+	echo $tbUserAttributes."<br />";
+	echo $tbUserValues."<br />";
+	echo $tbUserRoleVar."<br />";
+	echo $tbSiteTVs."<br />";
 	
-	$newRolesID[] = $newId;
-	
-	// Assign each new field to a role!
+	echo "<form><input name='commit' value='yes' type='submit' /></form>";
 
-	// Insert new ID into roles
-	$roleData = array(
-					'tmplvarid' => $newId,
-					'roleid'	=> $roleid,
-					'rank' 		=> 0
-					);
-	print_r($roleData);
-	echo "<br />";
-	
-	$modx->db->insert($roleData, $tbUserRoleVar);	
-}
-
-echo "<h1>Done! Working</h1>";
-
-echo "<h1>New Role IDs</h1>";
-print_r($newRolesID);
-
-echo "<h1>Updating User Roles</h1>";
-// Change User Roles for each user
-$sql = "SELECT * FROM ".$tbUsers.";";
-$rs = $modx->db->query($sql);
-
-while ( $row = $modx->db->getRow($rs) )
-{
-	// Change role of each user
-	// This will mean inserting a record for users who haven't been backend updated 
-	// or changing the role for those that have!
-	
-	$sql1 = "SELECT * FROM ". $tbUserSettings . " WHERE `user` = ".$row['id']." AND setting_name='role';";
-	$rs1 = $modx->db->query($sql1);
-	$count = $modx->db->getRecordCount($rs1);
-	
-	if ( $count )
-	{
-		// Update
-		if ( $updateExistingRoles == 1 ) 
-		{
-			$roleUpdate = array(
-							'setting_value' => $roleid
-							);
-			// DO NOT UPDATE EXISTING ROLES AT ALL
-			// This affects the admin account!
-			// echo "Updating: ".$row['id']."<br />";
-			// $modx->db->update($roleUpdate, $tbUserSettings, 'user='.$row['id']);
-		}
-	} else {
-		// INSERT
-		$roleInsert = array(
-						'user' => $row['id'],
-						'setting_name' => 'role',
-						'setting_value' => $roleid
-						);
-		echo "Inserting: ".$row['id']."<br />";
-		$modx->db->insert($roleInsert, $tbUserSettings);
-	}
-	
-	// Update user_attributes Role
-	if ( $row['id'] != $adminid )
-	{
-		$roleUpdate = array( "role" => $roleid);
-		$modx->db->update($roleUpdate, $tbUserAttributes, "internalKey = ".$row['id']);
-	}
-}
-
-echo "<h1>Done!</h1>";
-
-echo "<h1>Creating User TV Values!</h1>";
-
-// For each user role var we need to update user values accordingly
-// for each role(field) we need to populate the value from extended attributes table
-$i = 0;
-foreach ( $fields as $field )
-{
-	// first field to first role
-	$sql = "SELECT internalKey, ".$field. " FROM ". $tbExtended.";";
+	// Get the extended attribute field names and create TVs
+	$sql = "SHOW COLUMNS FROM ". $tbExtended .";";
 	$rs = $modx->db->query($sql);
-	//echo "<strong>".$sql."</strong><br />";
-	
+
 	while ( $row = $modx->db->getRow($rs) )
 	{
-		//Insert TV data for each field for each user!
-		$insertTVValue = array(
-							'tmplvarid' => $newRolesID[$i],
-							'userid' => $row['internalKey'],
-							'value' => $row[$field]
-							);
-		//echo "Updating: ".$newRolesID[$i]. " User: ".$row['internalKey']."Value: ".$row[$field]."<br />";
-		$modx->db->insert($insertTVValue,  $tbUserValues);
+		$fields[] = $row['Field'];
 	}
-	$i++;
-}
+	
+	unset($fields[0]);
+	unset($fields[1]);
 
-echo "<h1>Done!</h1>";
+	echo "<h1>Getting Fields</h1>";
+
+	print_r($fields);
+
+
+	// Create new TV's based on above field list:
+	echo "<h1>Inserting new TVs</h1>";
+	foreach ( $fields as $field )
+	{	
+		$insertflds = array(
+						'type' => 'text',
+						'name' => $field,
+						'caption' => $field,
+						'description' => $field,
+						'editor_type' => 0,
+						'category' => 0,
+						'editedon' => 0,
+						'createdon' => 0,
+						'locked' => 0);
+
+		print_r($insertflds);
+		echo "Inserting as TV's<br />";
+		
+		if ( $_GET['commit'] == 'yes' )
+		{
+			$modx->db->insert($insertflds, $tbSiteTVs );
+			$newId = $modx->db->getInsertId();
+		}
+		
+		$newRolesID[] = $newId;
+		
+		// Assign each new field to a role!
+
+		// Insert new ID into roles
+		$roleData = array(
+						'tmplvarid' => $newId,
+						'roleid'	=> $roleid,
+						'rank' 		=> 0
+						);
+		print_r($roleData);
+		echo "<br />";
+		
+		if ( $_GET['commit'] == 'yes' ) $modx->db->insert($roleData, $tbUserRoleVar);	
+	}
+
+	echo "<h1>Done! Working</h1>";
+
+	echo "<h1>New Role IDs</h1>";
+	print_r($newRolesID);
+
+	echo "<h1>Updating User Roles</h1>";
+	// Change User Roles for each user
+	$sql = "SELECT * FROM ".$tbUsers.";";
+	$rs = $modx->db->query($sql);
+
+	while ( $row = $modx->db->getRow($rs) )
+	{
+		// Change role of each user
+		// This will mean inserting a record for users who haven't been backend updated 
+		// or changing the role for those that have!
+		
+		$sql1 = "SELECT * FROM ". $tbUserAttributes . " WHERE `internalKey` = ".$row['id'].";";
+		$rs1 = $modx->db->query($sql1);
+		$count = $modx->db->getRecordCount($rs1);
+		
+		if ( $count )
+		{
+			// Update
+			$roleUpdate = array(
+							'role' => $roleid
+							);
+			echo "Updating: ".$row['username']." id ".$row['id']." to role ".$roleid."<br />";
+			if ( $_GET['commit'] == 'yes' ) $modx->db->update($roleUpdate, $tbUserAttributes, 'internalKey='.$row['id']);
+			
+		} 
+		/*
+		else {
+			// INSERT
+			$roleInsert = array(
+							'user' => $row['id'],
+							'setting_name' => 'role',
+							'setting_value' => $roleid
+							);
+			echo "Inserting: ".$row['id']."<br />";
+			if ( $_GET['commit'] == 'yes' ) $modx->db->insert($roleInsert, $tbUserSettings);
+		}
+		*/
+		
+		
+		// Update user_attributes Role
+		if ( $row['id'] != $adminid )
+		{
+			$roleUpdate = array( "role" => $roleid);
+			if ( $_GET['commit'] == 'yes' )  $modx->db->update($roleUpdate, $tbUserAttributes, "internalKey = ".$row['id']);
+		}
+	}
+
+	echo "<h1>Done!</h1>";
+
+	echo "<h1>Creating User TV Values!</h1>";
+
+	// For each user role var we need to update user values accordingly
+	// for each role(field) we need to populate the value from extended
+	$i = 0;
+	foreach ( $fields as $field )
+	{
+		// first field to first role
+		$sql = "SELECT internalKey, ".$field. " FROM ". $tbExtended.";";
+		$rs = $modx->db->query($sql);
+		//echo "<strong>".$sql."</strong><br />";
+		
+		while ( $row = $modx->db->getRow($rs) )
+		{
+			//Insert TV data for each field for each user!
+			$insertTVValue = array(
+								'tmplvarid' => $newRolesID[$i],
+								'userid' => $row['internalKey'],
+								'value' => $row[$field]
+								);
+			echo "Updating: ".$newRolesID[$i]. " User: ".$row['internalKey']."Value: ".$row[$field]."<br />";
+			
+			if ( $_GET['commit'] == 'yes' ) $modx->db->insert($insertTVValue,  $tbUserValues);
+		}
+		$i++;
+	}
+
+	echo "<h1>Done!</h1>";
+} else {
+	echo "<form><input name='update' value='view' type='submit' /></form> ";
+}
